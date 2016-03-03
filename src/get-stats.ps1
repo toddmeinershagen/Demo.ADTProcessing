1aeb2d9eac36f2ca90b73c01ad65b026e378bfc5
Todd Meinershagen <todd@meinershagen.net>
2016-03-02 18:46:58 -0600

    added to the statistics powershell script.

--- src/get-stats.ps1
+++ src/get-stats.ps1
@@ -2,6 +2,7 @@ $headers = @{}
 $headers.accept = "application/json"
 $waitingMessage = "waiting..."
 $hostName = "RCM41VQPERAPP41"
+# $hostName = "localhost"
 
 while ($true)
 {
@@ -12,12 +13,17 @@ while ($true)
 
         $avgRouterDelay = $router.avgDelayInMilliseconds
         $avgRouterExecution = $router.avgExecutionInMilliseconds
+        $routerMessageRate = $router.messagesPerSecond
+        $routerCount = [int]$router.count
         $avgDelay = $worker.avgDelayInMilliseconds
         $avgExecution = $worker.avgExecutionInMilliseconds
+        $workerMessageRate = $worker.messagesPerSecond
+        $workerCount = [int]$worker.count
 
         if ($avgDelay)
-        {     
-            "$avgrouterExecution ($avgRouterDelay) :: $avgExecution ($avgDelay)"       
+        {    
+            #$worker 
+            "router::$avgrouterExecution ms (dly=$avgRouterDelay ms, rt=$routerMessageRate/sec, ct={0:n0}) worker::$avgExecution ms (dly=$avgDelay ms, rt=$workerMessageRate/sec, ct={1:n0})" -f $routerCount, $workerCount       
         }
         else
         {
